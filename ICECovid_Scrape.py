49020256ef0d9734c6ccbf2c17b590ac62b9ab32
ivnagpal <63216494+ivnagpal@users.noreply.github.com>
2020-09-28 12:38:35 -0500

    Commit

--- ICECovid_Scrape.py
+++ ICECovid_Scrape.py
@@ -12,7 +12,6 @@ import datetime
 #Date and Time of Program
 dt = datetime.datetime.now().strftime('%m/%d/%y')
 
-
 #Parse ICE Website
 url = 'https://www.ice.gov/coronavirus#wcm-survey-target-id'
 response = requests.get(url) 
@@ -21,8 +20,8 @@ soup = BeautifulSoup(response.text, 'lxml')
 #Information on latest ICE Update
 p_list = soup.findAll ('p')
 p_list_txt = [txt.get_text() for txt in p_list]
-staff_update = p_list_txt[107].replace('Updated','').strip()
-detainee_update = p_list_txt[108].replace('Updated','').strip()
+staff_update = p_list_txt[81].replace('Updated','').strip()
+detainee_update = p_list_txt[84].replace('Updated','').strip()
 staff_update_string = 'ICE Employee Data Last Updated on:' + ' ' + staff_update
 detainee_update_string = 'ICE Detainee Data Last Updated on:' + ' ' + detainee_update
 print (staff_update_string)
